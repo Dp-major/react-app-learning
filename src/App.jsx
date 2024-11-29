0e32f3c25a387aac70d49bd5ef7b610ce555c427
Rishabh Bajpai <rishabh@Rishabhs-MacBook-Air.local>
2024-11-30 04:17:43 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
+import Header from './componnets/Header'
+import CoreConcept from './componnets/CoreConcept'
+import TabButton from './componnets/TabButton';
+
+function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState('components');
+
+  function handleSelect(selctedButton) {
+    // selectedButton => 'componenets', 'jsx', 'props', 'state
+    setSelectedTopic(EXAMPLES[`${selctedButton}`])
+    //console.log(selectedTopic);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image} 
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Exapmles</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            <h3>{selectedTopic.title}</h3>
+            <p>{selectedTopic.description}</p>
+            <pre>
+              <code>
+                {selectedTopic.code}
+              </code>
+            </pre>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
