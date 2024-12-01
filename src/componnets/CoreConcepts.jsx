83a7ac4ff377ffe5f9928ec5a02f8c84052987e4
Rishabh Bajpai <rishabh@Rishabhs-MacBook-Air.local>
2024-12-02 04:29:12 +0530

    Formatted components folder files with preetier

--- src/componnets/CoreConcepts.jsx
+++ src/componnets/CoreConcepts.jsx
@@ -2,12 +2,14 @@ import CoreConcept from "./CoreConcept";
 import { CORE_CONCEPTS } from "../data";
 
 export default function CoreConcepts() {
-    return (
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
-          </ul>
-        </section>
-    );
+	return (
+		<section id="core-concepts">
+			<h2>Core Concepts</h2>
+			<ul>
+				{CORE_CONCEPTS.map((conceptItem) => (
+					<CoreConcept key={conceptItem.title} {...conceptItem} />
+				))}
+			</ul>
+		</section>
+	);
 }
