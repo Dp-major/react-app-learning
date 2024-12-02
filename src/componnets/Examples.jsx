34499b8243db4e83fe68dd2b28de070b8834244a
Rishabh Bajpai <rishabh@Rishabhs-MacBook-Air.local>
2024-12-02 06:47:48 +0530

    setting component type dynamically (alternate)

--- src/componnets/Examples.jsx
+++ src/componnets/Examples.jsx
@@ -28,7 +28,7 @@ export default function Examples() {
   return (
     <Section id="examples" title="Examples" className="section">
       <Tabs
-        buttonsContainer="menu"
+        ButtonsContainer="menu"
         buttons={
           <>
             <TabButton
